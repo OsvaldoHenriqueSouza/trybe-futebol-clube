7f58fdc844c988cb34a970b755a1501634fd3527
OsvaldoHenriqueSouza <osvaldosouza_99@hotmail.com>
2022-10-31 11:55:40 -0300

    add migration e model de teams e matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,55 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+// import OtherModel from './OtherModel';
+
+class Matches extends Model {
+  declare id:number;
+  declare homeTeam:number;
+  declare homeTeamGoals:number;
+  declare awayTeam:number;
+  declare awayTeamGoals:number;
+  declare inProgress:boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Matches;
---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,19 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  teamName: { type: DataTypes.STRING, allowNull: false },
+}, {
+  sequelize: db,
+  underscored: true,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
